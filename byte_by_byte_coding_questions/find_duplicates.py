278bb5c75418f1e0a4c90332dc3db646037d3471
dmwangi <dmwangi@equitybank.co.ke>
2020-10-31 22:41:13 +0300

    Duplicates in an array

---
+++ byte_by_byte_coding_questions/find_duplicates.py
@@ -0,0 +1,38 @@
+def dups(arr):
+    # Return if empty or single item
+    if len(arr) <= 1:
+       return arr
+
+    temp_dict ={}
+    set_x = set()
+    n =  len(arr)
+
+    for i in range(0,n):
+        val = arr[i]
+        if val in temp_dict:
+            temp_dict[val] = int(temp_dict.get(val)) + 1
+        else:
+            temp_dict[val] = 1   
+
+        if int(temp_dict.get(val)) >= 2:
+            set_x.add(val)
+
+    return list(set_x)    
+
+
+
+
+x = dups([])
+print(x)
+
+x = dups([1,2,3])
+print(x)
+
+x = dups([1,2,2])
+print(x)
+
+x = dups([3,3,3,3])
+print(x)
+
+x = dups([2,1,2,1,3,3,3])
+print(x)
